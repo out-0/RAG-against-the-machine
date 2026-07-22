c997501ae11172da8cd89e2c04add493d823cc32
out <ayoubarid000@gmail.com>
2026-07-22 13:04:57 +0100

    Simplifing by removing unnecessary parts

--- src/indexer.py
+++ src/indexer.py
@@ -1,96 +1,70 @@
+"""Index class wrapper for handling choosed chunking method,
+
+indexing is basically create a special map that link each
+word to the relevant docs for it after scoring it by a
+some formules based on the indexing method,
+
+To simple it, Index the docs internally build a map 'the inverted index'
+which map a word to list of matching documents and the frequency
+of that word withing that document, something like:
+"vllm"      -----> [ Doc 0, Doc 2 ]
+"paged"     -----> [ Doc 0, Doc 1 ]
+"uses"      -----> [ Doc 0 ]
+"attention" -----> [ Doc 0, Doc 1 ]
+
+Its more than that but,
+also that is just implied for a lexical indexing like TF-IDF, BM25,
+But for an embedding its relay on a vectors which kida based on
+the attensions between the terms or phrases
+"""
+
 from src.docs_chunking import Chunk
 import bm25s
 from pathlib import Path
 import pickle
 
 
-class Indexer:
-    """Index class wrapper for handling choosed chunking method,
-
-    indexing is basically create a special map that link each
-    word to the relevant docs for it after scoring it by a
-    some formules based on the indexing method,
-
-    To simple it, Index the docs internally build a map 'the inverted index'
-    which map a word to list of matching documents and the frequency
-    of that word withing that document, something like:
-    "vllm"      -----> [ Doc 0, Doc 2 ]
-    "paged"     -----> [ Doc 0, Doc 1 ]
-    "uses"      -----> [ Doc 0 ]
-    "attention" -----> [ Doc 0, Doc 1 ]
-
-    Its more than that but,
-    also that is just implied for a lexical indexing like TF-IDF, BM25,
-    But for an embedding its relay on a vectors which kida based on
-    the attensions between the terms or phrases
+def indexing(
+    chunks: list[Chunk] | None = None,
+    processed_path: str = "data/processed",
+    method: str = "bm25",
+) -> None:
+    """Run the indexer based on the indexing method specified by args
+    if not specified, the default is bm25 which is implemented by
+    bm25s library
     """
 
-    def __init__(
-        self,
-        chunks: list[Chunk] | None = None,
-        method: str = "bm25",
-        processed_path: str = "data/processed",
-    ) -> None:
-        """Setup indexer to index the chunks of docs and build
-        the lookup table, which can be used later again the query
-        to retrieve the top k docs that relavant to that query
+    if not chunks:
+        raise TypeError("Warrning: Chunks to index is missed")
 
-        Args:
+    # Create the path for the indexed lookup
+    Path(processed_path).mkdir(parents=True, exist_ok=True)
 
+    # Extract content from chunks instead of full obj
+    docs: list[str] = [chunk.content for chunk in chunks]
 
-        Returns:
+    if method == "bm25":
+        # skipped stemming to avoid mangling code identifiers;
+        # may cost a small amount of recall
+        # stemmer = Stemmer.stemmer("english")
 
-        """
-
-        if not chunks:
-            raise TypeError("Warrning: Chunks to index is missed")
-
-        self.chunks: list[Chunk] = chunks
-        self.processed_path: str = processed_path
-        self.method: str = method
-
-        # Run the main index processing
-        self.run()
-
-    def run(self) -> None:
-        """Run the indexer based on the indexing method specified by args
-        if not specified, the default is bm25 which is implemented by
-        bm25s library
-        """
-
-        # Create the path for the indexed lookup
-        Path(self.processed_path).mkdir(parents=True, exist_ok=True)
-
-        # Extract content from chunks instead of full obj
-        docs: list[str] = [chunk.content for chunk in self.chunks]
-
-        if self.method == "bm25":
-            # skipped stemming to avoid mangling code identifiers;
-            # may cost a small amount of recall
-            # stemmer = Stemmer.stemmer("english")
-
-            corpus_tokens: list[list[str]] | bm25s.tokenization.Tokenized = (
-                bm25s.tokenize(texts=docs, show_progress=True)
-            )
-
-            retriever: bm25s.BM25 = bm25s.BM25()
-            retriever.index(corpus=corpus_tokens, show_progress=True)
-            retriever.save(self.processed_path)
-
-            # Save chunks as pickle file (binary format) so can
-            # be loaded also later to map result to chunk obj
-            with open(Path(self.processed_path) / "chunks.pkl", "wb") as f:
-                pickle.dump(self.chunks, f)
-
-        elif (
-            self.method == "embedding"
-        ):  # TODO: CHECK AND IMPLEMENT FOR EMBEDDING LATER
-            pass
-
-        print(
-            f"Ingestion complete! Indexed {len(self.chunks)} "
-            f"chunks under {self.processed_path}"
+        corpus_tokens: list[list[str]] | bm25s.tokenization.Tokenized = bm25s.tokenize(
+            texts=docs, show_progress=True
         )
+
+        retriever: bm25s.BM25 = bm25s.BM25()
+        retriever.index(corpus=corpus_tokens, show_progress=True)
+        retriever.save(processed_path)
+
+        # Save chunks as pickle file (binary format) so can
+        # be loaded also later to map result to chunk obj
+        with open(Path(processed_path) / "chunks.pkl", "wb") as f:
+            pickle.dump(chunks, f)
+
+    elif method == "embedding":  # TODO: CHECK AND IMPLEMENT FOR EMBEDDING LATER
+        pass
+
+    print(f"Ingestion complete! Indexed {len(chunks)} chunks under {processed_path}")
 
 
 # class Tf_idf_search:
